50bc3ce5a81377630e680a10bfee7d1a3eeb5ed4
Lars Brinkhoff <lars@nocrew.org>
2015-04-21 09:24:58 +0200

    Detect end of search results.

--- scrape.sh
+++ scrape.sh
@@ -1,14 +1,23 @@
-#QUERY="extension%3Afor+weekly+-language%3Aformatted"
-QUERY="dup+swap+language%3Afortran"
+QUERY="extension:scr+dup"
 > TMP
-for i in `seq 1 75`; do
-  echo "$i " 1>&2
-  URL="https://github.com/search?p=$i&q=$QUERY&type=Code"
-  curl "$URL" 2> /dev/null |
-    grep /blob/ | grep -v '#' | awk -F'"' '{print $2}' | cut -d/ -f1-3 >> TMP
-  sleep 10
-done
+
+fetch() {
+  i=1
+  while :; do
+    echo -n "$i " 1>&2
+    URL="https://github.com/search?p=$i&q=$QUERY&type=Code"
+    curl "$URL" > tmp.html 2> /dev/null
+    awk -F'"' '/\/blob\// && !/#/ {print $2}' < tmp.html | cut -d/ -f1-3 >> TMP
+    grep 'next_page disabled' tmp.html > /dev/null && return
+    i=$[$i + 1]
+    sleep 10
+  done
+}
+
+fetch; echo
 
 cat TMP | while read i; do
   echo "https://github.com$i"
 done | sort -u > query.csv
+
+rm -f TMP tmp.html
