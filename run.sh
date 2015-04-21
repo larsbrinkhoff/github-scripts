c82ef2bb2af3edf2b1accf374acaf5f63ddf7381
Lars Brinkhoff <lars@nocrew.org>
2015-04-21 09:24:21 +0200

    Output filenames relative base directory.

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@ test -n "`find samples -newer $samples`" && bundle exec rake samples
 test -d "$1/.git" && bundle exec linguist "$1"
 test -z "$2" && exit 0
 
-find "$1" -name .git -prune -o -type f -iname "*.$2" -print | while read i; do
-  echo -n "$i: "
-  bundle exec linguist "$i" | awk '/language:/ { print $2 }'
-done
+pwd=$PWD
+(cd "$1"
+ find . -name .git -prune -o -type f -iname "*.$2" -print | while read i; do
+   echo -n "$i: "
+   (cd $pwd; bundle exec linguist "$1/$i" | awk '/language:/ { print $2 }')
+ done)
