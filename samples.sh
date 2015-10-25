39c6fe9fd8f9f21917a43bf0a638f641f3a6be14
Paul Chaignon <paul.chaignon@gmail.com>
2015-10-25 21:05:42 +0100

    Search keywords for samples.sh as an argument

--- samples.sh
+++ samples.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-QUERY="extension:n+NOT+slartibartfast"
+folder=$2
+query=${1// /+}
+tmp_html="$folder.html"
+tmp_txt="$folder.txt"
 > TMP
 
 fetch() {
   i=1
   while :; do
     echo -n "$i " 1>&2
-    URL="https://github.com/search?p=$i&q=$QUERY&type=Code"
-    curl "$URL" > tmp.html 2> /dev/null
-    awk -F'"' '/\/blob\// && !/#/ {print $2}' < tmp.html >> TMP
-    grep 'next_page' tmp.html > /dev/null || return
-    grep 'next_page disabled' tmp.html > /dev/null && return
+    URL="https://github.com/search?p=$i&q=$query&type=Code"
+    curl "$URL" > $tmp_html 2> /dev/null
+    awk -F'"' '/\/blob\// && !/#/ {print $2}' < $tmp_html >> $tmp_txt
+    grep 'next_page' $tmp_html > /dev/null || return
+    grep 'next_page disabled' $tmp_html > /dev/null && return
     i=$[$i + 1]
-    sleep 7
+    sleep 10
   done
 }
 
 fetch; echo
 
-cat TMP | while read i; do
-  file=samples/`echo $i | cut -d/ -f1-3,6-`
+cat $tmp_txt | while read i; do
+  file="$folder/"`echo $i | cut -d/ -f1-3,6-`
   mkdir -p $(dirname $file)
   url="https://raw.githubusercontent.com`echo $i | sed 's/\/blob\//\//'`"
   curl "$url" 2> /dev/null > $file
 done
 
-rm -f TMP tmp.html
+rm -f $tmp_txt $tmp_html
